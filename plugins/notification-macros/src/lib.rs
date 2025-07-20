c185e8b0b00e3e224cfae53fc30ad866272f9d98
guillem.cordoba <guillem.cordoba@gmail.com>
2025-07-20 13:59:58 +0200

    Attempt

--- plugins/notification-macros/src/lib.rs
+++ plugins/notification-macros/src/lib.rs
@@ -54,6 +54,7 @@ pub fn receive_push_notification(_args: TokenStream, input: TokenStream) -> Toke
             // Initialize global context
             let context = env.new_global_ref(jobject).unwrap();
             let vm = env.get_java_vm().unwrap();
+            ndk_context::release_android_context();
             ndk_context::initialize_android_context(
               vm.get_java_vm_pointer() as *mut _,
               context.as_obj().as_raw() as *mut _,
