5c2b6d2717c78307b269cb6a5852f61f440848db
guillem.cordoba <guillem.cordoba@gmail.com>
2025-07-20 13:49:20 +0200

    Attempt

--- plugins/notification-macros/src/lib.rs
+++ plugins/notification-macros/src/lib.rs
@@ -54,7 +54,7 @@ pub fn receive_push_notification(_args: TokenStream, input: TokenStream) -> Toke
             // Initialize global context
             let context = env.new_global_ref(jobject).unwrap();
             let vm = env.get_java_vm().unwrap();
-            let env = vm.attach_current_thread_as_daemon().unwrap();
+            let mut env = vm.attach_current_thread_as_daemon().unwrap();
             ndk_context::initialize_android_context(
               vm.get_java_vm_pointer() as *mut _,
               context.as_obj().as_raw() as *mut _,
@@ -74,7 +74,7 @@ pub fn receive_push_notification(_args: TokenStream, input: TokenStream) -> Toke
 
             let jstring: jni::objects::JString = env.new_string(serde_json::to_string(&received_notification).expect("Can't serialize NotificationData").clone()).expect("Coulnd't reserve new string");
 
-            jstring
+            jstring.clone()
         }
 
         #[cfg(target_os = "ios")]
