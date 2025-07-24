429eb22a9b4f83cc3a25206cdaa6615360447a9e
guillem.cordoba <guillem.cordoba@gmail.com>
2025-07-24 13:28:35 +0200

    Fix

--- plugins/notification/src/lib.rs
+++ plugins/notification/src/lib.rs
@@ -226,39 +226,49 @@ impl<R: Runtime, T: Manager<R>> crate::NotificationExt<R> for T {
 }
 
 /// Initializes the plugin.
+#[cfg(all(mobile, feature = "push-notifications-fcm"))]
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
-    let mut builder = Builder::new("notification");
+    let mut builder = Builder::new("notification")
+        .invoke_handler(tauri::generate_handler![
+            commands::notify,
+            commands::request_permission,
+            commands::is_permission_granted,
+        ])
+        .js_init_script(include_str!("init-iife.js").replace(
+            "__TEMPLATE_windows__",
+            if cfg!(windows) { "true" } else { "false" },
+        ))
+        .setup(|app, api| {
+            #[cfg(mobile)]
+            let notification = mobile::init(app, api)?;
+            #[cfg(desktop)]
+            let notification = desktop::init(app, api)?;
+            app.manage(notification);
+            Ok(())
+        })
+        .build()
+}
 
-    #[cfg(all(mobile, feature = "push-notifications-fcm"))]
-    {
-        builder = builder.invoke_handler(tauri::generate_handler![
+#[cfg(not(all(mobile, feature = "push-notifications-fcm")))]
+pub fn init<R: Runtime>() -> TauriPlugin<R> {
+    let mut builder = Builder::new("notification")
+        .invoke_handler(tauri::generate_handler![
             commands::notify,
             commands::request_permission,
             commands::is_permission_granted,
             commands::get_launching_notification_action
-        ]);
-    }
-
-    #[cfg(not(all(mobile, feature = "push-notifications-fcm")))]
-    {
-        builder = builder.invoke_handler(tauri::generate_handler![
-            commands::notify,
-            commands::request_permission,
-            commands::is_permission_granted,
-        ]);
-    }
-
-    builder.js_init_script(include_str!("init-iife.js").replace(
-        "__TEMPLATE_windows__",
-        if cfg!(windows) { "true" } else { "false" },
-    ))
-    .setup(|app, api| {
-        #[cfg(mobile)]
-        let notification = mobile::init(app, api)?;
-        #[cfg(desktop)]
-        let notification = desktop::init(app, api)?;
-        app.manage(notification);
-        Ok(())
-    })
-    .build()
+        ])
+        .js_init_script(include_str!("init-iife.js").replace(
+            "__TEMPLATE_windows__",
+            if cfg!(windows) { "true" } else { "false" },
+        ))
+        .setup(|app, api| {
+            #[cfg(mobile)]
+            let notification = mobile::init(app, api)?;
+            #[cfg(desktop)]
+            let notification = desktop::init(app, api)?;
+            app.manage(notification);
+            Ok(())
+        })
+        .build()
 }
